7a11bf1e56d0d967d63d3afe774893f5519c0c45
p1gp1g <simon.gougeon@protonmail.com>
2020-11-25 10:47:03 +0100

    receive msg as json

--- src/main/java/com/github/gotify/connector/GotifyMessage.kt
+++ src/main/java/com/github/gotify/connector/GotifyMessage.kt
@@ -1,8 +1,6 @@
 package com.github.gotify.connector
 
-import android.os.Bundle
 import java.time.OffsetDateTime
-
 
 class GotifyMessage{
     var appid: Long? = null;
@@ -12,14 +10,4 @@ class GotifyMessage{
     var message: String? = null
     var priority: Long? = null
     var title: String? = null
-
-    fun initFromBundle(data: Bundle): GotifyMessage {
-        appid = data.getLong("appid")
-        id = data.getLong("id")
-        message = data.getString("message")
-        priority = data.getLong("priority")
-        title = data.getString("title")
-        return this
-    }
-
 }
--- src/main/java/com/github/gotify/connector/GotifyServiceHandler.kt
+++ src/main/java/com/github/gotify/connector/GotifyServiceHandler.kt
@@ -1,6 +1,7 @@
 package com.github.gotify.connector
 
 import android.os.*
+import com.google.gson.Gson
 
 /**
  * This handler is used to receive notifications
@@ -16,12 +17,13 @@ open class GotifyServiceHandler() : Handler(Looper.getMainLooper()) {
         }
         when (msg.what) {
             TYPE_CHANGED_URL -> onUrlChange(msg.data.getString("changedUrl").orEmpty())
-            TYPE_MESSAGE -> onMessage(GotifyMessage().initFromBundle(msg.data))
+            TYPE_MESSAGE -> onMessage(Gson().fromJson(
+                    msg.data?.getString("json"),
+                    GotifyMessage::class.java))
             else -> super.handleMessage(msg)
         }
     }
 
-    //open fun onMessage(message: Message){}
     open fun onMessage(message: GotifyMessage){}
 
     open fun onUrlChange(url: String) {}
