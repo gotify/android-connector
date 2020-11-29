5018f7e34acc22d70d6a40992c1823b017b59454
p1gp1g <simon.gougeon@protonmail.com>
2020-11-29 19:12:20 +0100

    GotifyMessage: data class

--- src/main/java/com/github/gotify/connector/GotifyMessage.kt
+++ src/main/java/com/github/gotify/connector/GotifyMessage.kt
@@ -2,12 +2,12 @@ package com.github.gotify.connector
 
 import java.time.OffsetDateTime
 
-class GotifyMessage{
-    var appid: Long? = null;
-    var date: OffsetDateTime? = null;
-    var extras: Map<String, Object>? = null
-    var id: Long? = null
-    var message: String? = null
-    var priority: Long? = null
-    var title: String? = null
-}
+data class GotifyMessage(
+    var appid: Long,
+    var date: OffsetDateTime,
+    var extras: Map<String, Object>,
+    var id: Long,
+    var message: String,
+    var priority: Long,
+    var title: String
+)
